4688ae1528e05691fe3f0f95a3fe090c1c723b40
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-03 21:57:17 +0300

    changed 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <div className="flex items-center justify-center w-full min-h-screen bg-gray-600">
+        <h1 className="text-4xl font-bold text-center text-white">
+          Hello World!
+        </h1>
+      </div>
     </>
   );
 }
