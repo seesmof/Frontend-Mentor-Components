94954cfdf80c5259eaa23cc5ef3b3e44cbca4b86
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-04 16:00:10 +0300

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,34 @@
 import Link from "next/link";
 import Card from "@/components/main/Card";
+import Navbar from "@/components/main/Navbar";
 
 export default function Home() {
   return (
     <>
       <div className="min-h-screen p-4 md:p-6 bg-slate-900 text-slate-100">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 mx-auto max-w-4xl">
-          <Card link="/chat-app-css-illustration" name="Chat App" />
-          <Card link="/clipboard-landing-page" name="Clipboard Landing Page" />
-          <Card link="/fylo-data-storage-component" name="Fylo Data Storage" />
-          <Card
-            link="/product-preview-card-component"
-            name="Product Preview Card"
-          />
-          <Card link="/qr-code-component" name="QR Code" />
-          <Card link="/testimonials-grid-section" name="Testimonials Grid" />
-          <Card
-            link="/newsletter-sign-up-with-success-message-component"
-            name="Newsletter Sign Up Form"
-          />
+        <div className="flex flex-col mx-auto max-w-4xl">
+          <Navbar />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 pt-4">
+            <Card link="/chat-app-css-illustration" name="Chat App" />
+            <Card
+              link="/clipboard-landing-page"
+              name="Clipboard Landing Page"
+            />
+            <Card
+              link="/fylo-data-storage-component"
+              name="Fylo Data Storage"
+            />
+            <Card
+              link="/product-preview-card-component"
+              name="Product Preview Card"
+            />
+            <Card link="/qr-code-component" name="QR Code" />
+            <Card link="/testimonials-grid-section" name="Testimonials Grid" />
+            <Card
+              link="/newsletter-sign-up-with-success-message-component"
+              name="Newsletter Sign Up Form"
+            />
+          </div>
         </div>
       </div>
     </>
---
+++ components/main/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+const Navbar = () => {
+  return (
+    <>
+      <div className="border-b-2 flex flex-row items-center justify-between border-slate-700 pb-2">
+        <h2 className="font-bold sm:text-xl">Frontend Mentor</h2>
+        <div className="flex flex-row items-center gap-2">
+          <a
+            href="https://github.com/seesmof/frontend-mentor-solutions"
+            target="_blank"
+            title="View Source Code on GitHub"
+            className="flex flex-row items-end gap-1"
+          >
+            <AiFillGithub className="text-2xl" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
