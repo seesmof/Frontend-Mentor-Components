03f5600e839793160149553f3b54785fd371fb42
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-04 11:19:18 +0000

    🌿: Friday, August 4, 2023 at 2:19:18 PM Eastern European Summer Time

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Card from "@/components/main/Card";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen p-4 md:p-6 bg-slate-900 text-slate-50">
+      <div className="min-h-screen p-4 md:p-6 bg-slate-900 text-slate-100">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 mx-auto max-w-4xl">
           <Card link="/chat-app-css-illustration" name="Chat App" />
           <Card link="/clipboard-landing-page" name="Clipboard Landing Page" />
@@ -15,6 +15,10 @@ export default function Home() {
           />
           <Card link="/qr-code-component" name="QR Code" />
           <Card link="/testimonials-grid-section" name="Testimonials Grid" />
+          <Card
+            link="/newsletter-sign-up-with-success-message-component"
+            name="Newsletter Sign Up Form"
+          />
         </div>
       </div>
     </>
